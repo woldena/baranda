ce6568a4b25b5d9595dcc57ecbd86ff4a078b5b4
Andre Wolden <wolden.andre@gmail.com>
2019-07-16 07:21:02 +0200

    cra som utgangspunkt

---
+++ README.md
@@ -0,0 +1,10 @@
+# BARANDA
+
+## Run locally
+git clone
+cd 
+npm install
+npm start
+
+## CMS
+All texts are located in public/texts
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Baranda</h3>
+    </div>
+  );
+}
+
+export default App;
