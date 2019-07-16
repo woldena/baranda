330f99e65b81c5405ce3e7efc7cbe73517d64c34
Andre Wolden <wolden.andre@gmail.com>
2019-07-16 07:33:36 +0200

    legger til en express server til bruk på heroku

---
+++ server/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+
+const port = process.env.PORT;
+var app = express();
+
+app.get("*/static/*", (req, res) => {
+    console.log(`request path: ${req.path}. serving: ${path.resolve(__dirname, "build/static", req.params[1])}`);
+    res.sendFile(path.resolve(__dirname, "build/static", req.params[1]))
+});
+
+
+app.get("*", (req, res) => {
+    console.log(`route  ${req.path}`);
+    res.sendFile(path.resolve(__dirname, "build/index.html"))
+});
+
+app.listen(port, (req, res) => {
+    console.log(`listening on port ${port}`)
+});
