6fd1e626de32eb60300359adb061f42e176ce2d0
Andre Wolden <wolden.andre@gmail.com>
2019-07-16 07:41:39 +0200

    docker setup for heroku

--- server/server.js
+++ server/server.js
@@ -5,14 +5,14 @@ const port = process.env.PORT;
 var app = express();
 
 app.get("*/static/*", (req, res) => {
-    console.log(`request path: ${req.path}. serving: ${path.resolve(__dirname, "build/static", req.params[1])}`);
-    res.sendFile(path.resolve(__dirname, "build/static", req.params[1]))
+    console.log(`request path: ${req.path}. serving: ${path.resolve(__dirname, "../build/static", req.params[1])}`);
+    res.sendFile(path.resolve(__dirname, "../build/static", req.params[1]))
 });
 
 
 app.get("*", (req, res) => {
     console.log(`route  ${req.path}`);
-    res.sendFile(path.resolve(__dirname, "build/index.html"))
+    res.sendFile(path.resolve(__dirname, "../build/index.html"))
 });
 
 app.listen(port, (req, res) => {
